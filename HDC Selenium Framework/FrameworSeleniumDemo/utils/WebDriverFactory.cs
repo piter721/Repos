08a1c334925a72359b7655a4c0db4b5692f582f2
Pedro Andres Perez Aballay <paperez@hogardecristo.cl>
2021-09-02 09:51:10 -0400

    Se ingresa el framework de seleniumm

---
+++ HDC Selenium Framework/FrameworSeleniumDemo/utils/WebDriverFactory.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace FrameworSeleniumDemo.utils
+{
+    public class WebDriverFactory
+    {
+        public IWebDriver driver { get; set; }
+    }
+}
